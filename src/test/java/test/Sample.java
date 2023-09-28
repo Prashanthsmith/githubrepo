c662f017d50be7494cfb4687e67c2f2a739fc31c
prashanthfour9@gmail.com <prashanthfour9@gmail.com>
2023-09-28 15:38:23 +0530

    Second commit

---
+++ src/test/java/test/Sample.java
@@ -0,0 +1,12 @@
+package test;
+
+import org.testng.annotations.Test;
+
+public class Sample {
+	@Test 
+	public void sample() {
+		System.out.println("Sample");
+	}
+	
+
+}
---
+++ src/test/java/test/sampele.java
@@ -0,0 +1,11 @@
+package test;
+
+import org.testng.annotations.Test;
+
+public class sampele {
+
+	@Test
+	public void simple() {
+		System.out.println("Finally");
+	}
+}
